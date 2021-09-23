18325e66c104b7bd4e88df62ea1002be6063846a
Brad Trnavsky <bmtrnavsky@msn.com>
2021-09-23 14:56:54 -0500

    This is for taking notes related to numbers in python.

---
+++ Notes_on Python3_numbers.py
@@ -0,0 +1,28 @@
+# You can add (+) subtract (-) multiply (*) and divide (/) integers in Python
+
+print(2+3)
+print(2-3)
+print(2*3)
+print(2/3)
+
+# Two multiplacation symbols = an exponent
+print(3**2)
+print(3**3)
+print(10**6)
+
+# python also supports order of operations using PEMDAS
+
+print(2+3*4)
+print((2+3)*4)
+
+# Python also supports floats or decimal numbers. 
+print(0.1+0.1)
+print(2*0.2)
+
+#integers and floats. When you divide any two numbers you always get a float.
+print(4/2)
+
+# If you mix floats and integers you wil also get a float. 
+print(2+3.0)
+print(2*3.0)
+print(2.0**3)
